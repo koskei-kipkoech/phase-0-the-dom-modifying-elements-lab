6bafb7de5b0c9967b27d580629778a2802776250
Patrick Kipkoech <patrickwayy@gmail.com>
2024-09-23 10:55:32 +0300

    Updates

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.getElementById("main").remove();
+window.newHeader = document.createElement("h1");
+window.newHeader.id = "victory"
+window.newHeader.textContent ="Patrick is the champion"
+document.body.appendChild(window.newHeader);
